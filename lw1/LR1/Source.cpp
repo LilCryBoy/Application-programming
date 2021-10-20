08e68b65a1555b9f23680e2268e86b5d038dba11
LCB <temagastudio@gmail.com>
2021-10-20 19:57:14 +0500

    add files

---
+++ lw1/LR1/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+
+
+int main() 
+{
+	double PI = 3.14159265;
+	double y, z;
+	y = 2 * (pow(sin(3 * PI), 2)) * (pow(cos(5 * PI), 2));
+	z = 1/4 - 1/4 * sin(5/2 * PI - 
+	std::cout << y << std::endl;
+	system("pause");
+}
