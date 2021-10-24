0c458c46319fae652ea11504292dfbf6bf544490
LCB <temagastudio@gmail.com>
2021-10-24 17:28:59 +0500

    DELETE

--- PW1/LR1/Source.cpp
+++ PW1/LR1/Source.cpp
@@ -5,10 +5,11 @@
 
 int main() 
 {
-	double PI = 3.14159265;
-	double y, z;
-	y = 2 * (pow(sin(3 * PI), 2)) * (pow(cos(5 * PI), 2));
-	z = 1/4 - 1/4 * sin(5/2 * PI - 
+	/*double PI = 3.14159265;
+	double y, z, a = 0;
+	y = 2 * pow(sin(3 * PI - 2 * a), 2) * pow(cos(5 * PI + 2 * a), 2);
+	z = 1 / 4 - 1 / 4 * sin(5 / 2 * PI - 8 * a);
 	std::cout << y << std::endl;
+	std::cout << z << std::endl;*/
 	system("pause");
 }
